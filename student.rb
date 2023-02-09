0b2e00e7e811a4ed8d5711efd95935efd265cd61
sebalp100 <sebalp100@gmail.com>
2023-02-09 18:07:02 -0300

    Add new classes and classroom association

--- student.rb
+++ student.rb
@@ -1,11 +1,18 @@
 require_relative 'person'
+require_relative 'classroom'
 
 class Student < Person
   attr_accessor :name, :age, :classroom
+  attr_reader :label
 
   def initialize(name, age, classroom, parent_permission: true)
     super(name, age, parent_permission: parent_permission)
     @classroom = classroom
+  end
+
+  def label=(label)
+    @label = label
+    label.student.push(self) unless label.student.include?(self)
   end
 
   def play_hooky
