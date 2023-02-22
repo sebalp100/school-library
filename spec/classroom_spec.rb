77f4a3f992824c3dcfc9ca8cf98f77ff0e28ea84
enoqueJonas <enoque.macanda@gmail.com>
2023-02-22 19:01:05 +0200

    ADD unit test f/ the method add_stundent

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -8,4 +8,12 @@ describe Classroom do
     it 'Should create a new instance of Classroom' do
         expect(@classroom).to be_instance_of Classroom
     end
+
+    it 'Should add a student to a classroom' do
+        student = double('student')
+        allow(student).to receive(:classroom=)
+        @classroom.add_student student
+
+        expect(@classroom.students).to eql [student]
+    end
 end
