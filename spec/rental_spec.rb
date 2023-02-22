8897153c243f7f17fa2032dce9a14dc3ef037622
enoqueJonas <enoque.macanda@gmail.com>
2023-02-22 19:20:07 +0200

    FIX linters

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,13 +1,14 @@
 require_relative 'spec_helper'
 
 describe Rental do
-    it 'Should create an instance of the Rental class' do
-        @book = double('book')
-        @person = double('person')
-        allow(@book).to receive(:rentals).and_return([])
-        allow(@person).to receive(:rentals) { [] }
-        @rental  = Rental.new @person, @book, '12-12-1999'
+  it 'Should create an instance of the Rental class' do
+    @book = double('book')
+    @person = double('person')
 
-        expect(@rental).to be_instance_of Rental
-    end
+    allow(@book).to receive(:rentals).and_return([])
+    allow(@person).to receive(:rentals) { [] }
+    @rental = Rental.new @person, @book, '12-12-1999'
+
+    expect(@rental).to be_instance_of Rental
+  end
 end
